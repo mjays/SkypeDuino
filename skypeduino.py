e77f29692973f745fcb7c76ccb15eebc1a011ae9
mjays <martin@mjays.net>
2013-02-25 17:56:58 +0100

    Arduino listens to CallStatus and Mute

--- skypeduino.py
+++ skypeduino.py
@@ -3,25 +3,43 @@ import sys
 import serial
 
 class MySkypeEvents:
-    def UserStatus(self, Status):
-        print 'The status of the user changed to ' + Status
-        if Status == 'ONLINE':
-        	s.write(b'g')
-        elif Status == 'AWAY':
-        	s.write(b'y')
-        elif Status == 'DND':
-        	s.write(b'r')
-        else:
-        	s.write(b'0')
+  def CallStatus(self, Call, Status):
+    print 'The Call status has changed to ' + Status
+    if Status == 'INPROGRESS':
+      s.write(b'r')
+    elif Status == 'FINISHED':
+      s.write(b'g')
+    elif Status == 'CANCELLED':
+      s.write(b'g')
+    elif Status == 'RINGING':
+      s.write(b'y')
+    elif Status == 'UNPLACED':
+      s.write(b'y')
+    elif Status == 'ROUTING':
+      s.write(b'y')
+    elif Status == 'EARLYMEDIA':
+      s.write(b'y')
+    else:
+      s.write(b'g')
+  
+  def Mute(self, Mute):
+    print 'Current Mic status: ' + str(Mute)
+  
+  def Notify(self, Notification):
+    if Notification == 'MUTE OFF':
+      print 'Signaling MUTE OFF'
+      s.write(b'r')
+    elif Notification == 'MUTE ON':
+      print 'Signaling MUTE ON'
+      s.write(b'y')
 
-
-s = serial.Serial(port='/dev/tty.usbserial-A7006Qu2', baudrate=9600)
+s = serial.Serial(port='/dev/tty.usbserial-A7006TaZ', baudrate=9600)
 
 skype = Skype4Py.Skype(Events=MySkypeEvents())
 skype.FriendlyName = 'Skype4Py_Example'
 skype.Attach()
-while 1:
-	print "idling"
-
 print "Attachment status is " + str(skype.AttachmentStatus)
+
+while 1:
+  i = 1
 
